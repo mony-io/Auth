9c951110bb674bb39ec07bd45195d976a0799be0
mony-io <rysarakmony6101@gmail.com>
2022-10-04 13:26:16 +0700

    Authencation

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
+
+function App() {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        {!authCtx.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path='/profile'>
+            <UserProfile />
+          </Route>
+        )}
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Profile/ProfileForm.js
@@ -0,0 +1,49 @@
+import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import AuthContext from '../../store/auth-context';
+import classes from './ProfileForm.module.css';
+
+const ProfileForm = () => {
+  const history = useHistory();
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB1ey5B-146dkm3O3daxF5rFrv3fbaL-3Q', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => {
+      // assumption always succeeds!
+
+      history.replace('/');
+    })
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input type='password' id='new-password' minLength='7' ref={newPasswordInputRef} />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
